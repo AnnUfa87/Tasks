a1a6a8f937449925e881ac2e53938b47f82cd4c6
Иванова Анна <babankak174@mail.ru>
2022-09-22 20:21:45 +0500

    Second task

---
+++ Second task/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+byte num_1, num_2, num_3;
+Console.WriteLine("Введите первое число: ");
+num_1 = Convert.ToByte(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+num_2 = Convert.ToByte(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+num_3 = Convert.ToByte(Console.ReadLine());
+
+if(num_1 > num_2 && num_1 > num_3) {
+    Console.WriteLine("Максимальное из трех чисел равно:" + num_1);
+} 
+else if(num_2 > num_1 && num_2 > num_3) {
+   Console.WriteLine("Максимальное из трех чисел равно:" + num_1);
+}
+else if(num_3 > num_1 && num_3 > num_2) {
+    Console.WriteLine("Максимальное из трех чисел равно:" + num_3);
+}
+
+
+
+
+
+
