a4c2aac436b2b7faa42ea7b0d615c623a9db14d1
Иванова Анна <babankak174@mail.ru>
2022-09-22 20:23:25 +0500

    Fourth task

---
+++ Fourth task/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные 
+//числа от 1 до N.
+
+int a;
+Console.WriteLine("Введите любое число ");
+a = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+
+while (result < a){
+    Console.WriteLine(result+1);
+    result+=2;
+}
